8fb16865687aff6c5a8ff7cf4c7487f684d1988d
miguelalverga <miguelalverga2006@gmail.com>
2025-08-20 10:59:53 -0300

    Prova

---
+++ Aluno.java
@@ -0,0 +1,63 @@
+public class Aluno {
+    private String nome ;
+    private int matricula;
+    private  double nota1;
+    private double nota2;
+
+    public Aluno(String nome, int matricula) {
+        this.nome =nome;
+        this.matricula = matricula;
+        this.nota1 = 0.0;
+        this.nota2 = 0.0;
+    }
+
+    public String getNome () {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getMatricula() {
+        return matricula;
+    }
+    
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double calcularMedia() {
+        return (nota1 + nota2) / 2.0;
+    }
+
+    public boolean estaAprovado() {
+        return calcularMedia() >= 6.0;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println( "Matricula:" + matricula);
+        System.out.println("Nota 1:" + nota1);
+        System.out.println("Nota 2:" +nota2);
+        System.out.println("Média:" + calcularMedia());
+        System.out.println("Aprovado:" + (estaAprovaddo() ? "Sim" : "Não"));
+    
+    } 
+       
+    }
+
